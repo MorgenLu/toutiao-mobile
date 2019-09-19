f3b764cc5c5e2057c06cfe0f7c995f0f238d9dcd
卢一斌 <850378029@qq.com>
2019-09-19 11:18:18 +0800

    登录界面样式结构

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/'
 import store from './store/'
+import './styles/index.css'
 
+import {
+  NavBar,
+  Field,
+  Button
+} from 'vant'
 Vue.config.productionTip = false
 
+Vue.use(NavBar)
+  .use(Field)
+  .use(Button)
 new Vue({
   router,
   store,
